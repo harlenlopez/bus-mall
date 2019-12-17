00de245ad04829824b5167e04d68b71f855a5bd3
harlen lopez <harlen.d.lopez@gmail.com>
2019-12-17 11:34:50 -0800

    a little more added

--- app.js
+++ app.js
@@ -6,31 +6,18 @@ var imageDownloads = document.getElementsByTagName('img');
 var imgIndex1 = 0;
 var imgIndex2 = 1;
 var imgIndex3 = 2;
-var imgIndex4 = 3;
-// var imgIndex5 = 4;
-// var imgIndex6 = 5;
-// var imgIndex7 = 6;
-// var imgIndex8 = 7;
-// var imgIndex9 = 8;
-// var imgIndex10 = 9;
-// var imgIndex11= 10;
-// var imgIndex12= 11;
-// var imgIndex13= 12;
-// var imgIndex14= 13;
-// var imgIndex15= 14;
-// var imgIndex16= 15;
-// var imgIndex17= 16;
-// var imgIndex18= 17;
 
 var allImages = [];
 
-function image(name, imageURL){
+function image(name, imageURL, timesClicked) {
   this.name = name;
   this.imageURL = imageURL;
   this.timesClicked = 0;
+  this.imageViewed = 0;
   allImages.push(this);
 
 }
+console.log('function');
 
 new image('bag', 'img/bag.jpg');
 new image('banana', 'img/banana.jpg');
@@ -52,28 +39,40 @@ new image('water-can', 'img/water-can.jpg');
 new image('wine-glass', 'img/wine-glass.jpg');
 
 var totalTimesClicked = 0;
-function imageClicked(event){
+function imageClicked(event) {
   console.log('clicked on image');
   totalTimesClicked++;
-  if(event.srcElement.id === '1') {
-allImages[imgIndex1].totalTimesClicked++;
+  if (event.srcElement.id === '1') {
+    allImages[imgIndex1].totalTimesClicked++;
   } else if (event.srcElement.id === '2') {
     allImages[imgIndex2].totalTimesClicked++;
   } else if (event.srcElement.id === '3') {
     allImages[imgIndex3].totalTimesClicked++;
   }
-} 
+}
+console.log('things');
+// debugger;
+var ranNum = Math.floor(Math.random() * allImages.lenght);
+var currArray = [];
+var pasArray = [];
+while (currArray.lenght < 3) {
+  if (!pasArray.contains(ranNum));
+  if (!currArray.contains(ranNum));
 
+}
+console.log('array');
+
+// turn into a function that i can call, get rid of index,
 var nextImgIndex1 = Math.floor(Math.random() * allImages.length);
-while ((nextImgIndex2 ===imgIndex1) || (nextImgIndex1 === imgIndex2)){
+while ((nextImgIndex2 === imgIndex1) || (nextImgIndex1 === imgIndex2)) {
   nextImgIndex1 = Math.floor(Math.random() * allImages.length);
 }
 var nextImgIndex2 = Math.floor(Math.random() * allImages.length);
-while((nextImgIndex2 === imgIndex1) || (nextImgIndex2 === imgIndex2) || (nextImgIndex2 === nextImgIndex1===)) {
+while ((nextImgIndex2 === imgIndex1) || (nextImgIndex2 === imgIndex2) || (nextImgIndex2 === nextImgIndex1)) {
   nextImgIndex2 = Math.floor(Math.random() * allImages.length);
 }
 var nextImgIndex3 = Math.floor(Math.random() * allImages.length);
-while((nextImgIndex3 === imgIndex1) || (nextImgIndex3 === imgIndex2) || (nextImgIndex3 === nextImgIndex1 === nextImgIndex2)) {
+while ((nextImgIndex3 === imgIndex1) || (nextImgIndex3 === imgIndex2) || (nextImgIndex3 === nextImgIndex1 === nextImgIndex2)) {
   nextImgIndex3 = Math.floor(Math.random() * allImages.length);
 }
 imgIndex1 = nextImgIndex1;
@@ -84,12 +83,18 @@ imageDownloads[0].src = allImages[imgIndex1].imageURL;
 imageDownloads[1].src = allImages[imgIndex2].imageURL;
 imageDownloads[2].src = allImages[imgIndex3].imageURL;
 
-if(totalClicks >=25){
-  var footerElement = document.getElementsByClassName('footer')[0];
-  footerElement.textContent = `Thank you for assisting us in choosing a new product`;
-}
-}
-for (var i = 0; i<imageDownloads.length; i++) {
+
+// if (totalTimesClicked >= 25) {
+//   var footerElement = document.getElementsByClassName('footer')[0];
+//   footerElement.textContent = `Thank you for assisting us in choosing a new product`;
+//   for (var i = 0; i < imageElements.lenght; i++){
+//     imageElements[i].removeEventListener('click', imageWasClicked);
+//   }
+//   makeList();
+//   }
+// // };
+
+for (var i = 0; i < imageDownloads.length; i++) {
   imageDownloads[i].addEventListener('click', imageClicked);
 }
 
@@ -109,6 +114,14 @@ for (var i = 0; i<imageDownloads.length; i++) {
 
 
 
-function images(){
+// function images() {
 
-}
+// }
+
+//day 2 new stuff to add
+//
+//
+//adding onto existing base code
+//using chart.js
+//pulling in external code into exesting code
+//gonna use a cdn to pull in our files, its a contant delivery
